944395c008292d923343f9fce80c8dcac074d8f2
ahmad ilham <ahmad.ilham@amartha.com>
2024-09-26 14:48:29 +0700

    update setup exclude test

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from firebase_browserstack import __version__
 setup(
     name='firebase_browserstack',
     version=__version__,
+    url="https://github.com/ahmadachmed/firebase_browserstack",
     author='ahmadachmed',
     author_email='ahmadilham000@gmail.com',
     description='Firebase app distribution integrated with Browserstack',
-    packages= find_packages(),
+    packages= find_packages(exclude=('test', 'test.*')),
     install_requires=[
-        'google-api-python-client',
-        'requests'
+        'google-api-python-client'
     ]
 )
